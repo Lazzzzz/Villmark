c81a8d6fc26155cda698928cc08cbc9d9063df36
Drizzs <drizzs@hotmail.com>
2019-12-12 08:58:17 -0500

    Added Blocks

--- mod-1.14/src/main/java/laz/villmark/content/VillMarkBlocks.java
+++ mod-1.14/src/main/java/laz/villmark/content/VillMarkBlocks.java
@@ -22,23 +22,73 @@ public class VillMarkBlocks {
 	private static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, MOD_ID);
 	public static List<RegistryObject<Block>> blocklist = new ArrayList<RegistryObject<Block>>();
 
-	//Stone
+	//VillStone
 	public static final RegistryObject<Block> VILLSTONE = BLOCKS.register("villstone", () -> new Block(Block.Properties.create(ROCK)
 			.sound(SoundType.STONE)
 			.hardnessAndResistance(1.5f, 30f)
 	));
+	public static final RegistryObject<Block> VILLMARK_BEDROCK = BLOCKS.register("villmark_bedrock", () -> new Block(Block.Properties.create(ROCK)
+			.sound(SoundType.STONE)
+			.hardnessAndResistance(1.5f, 30f)
+	));
+	public static final RegistryObject<Block> VILLSAND = BLOCKS.register("villsand", () -> new Block(Block.Properties.create(SAND)
+			.sound(SoundType.SAND)
+			.hardnessAndResistance(0.5f, 10f)
+	));
+	public static final RegistryObject<Block> VILLSANDROCK = BLOCKS.register("villsandrock", () -> new Block(Block.Properties.create(ROCK)
+			.sound(SoundType.STONE)
+			.hardnessAndResistance(1.5f, 30f)
+	));
+
+
 	public static final RegistryObject<Block> DRYROCK = BLOCKS.register("dryrock", () -> new Block(Block.Properties.create(ROCK)
 			.sound(SoundType.STONE)
 			.hardnessAndResistance(1f, 20f)
 	));
-	public static final RegistryObject<Block> DREADBONE = BLOCKS.register("dreadbone", () -> new Block(Block.Properties.create(ORGANIC)
+	public static final RegistryObject<Block> DRYROCK_BRICKS = BLOCKS.register("dryrock_bricks", () -> new Block(Block.Properties.create(ROCK)
 			.sound(SoundType.STONE)
-			.hardnessAndResistance(1f, 10f)
+			.hardnessAndResistance(1f, 20f)
 	));
+
+	//FleshStone
 	public static final RegistryObject<Block> FLESHSTONE = BLOCKS.register("fleshstone", () -> new Block(Block.Properties.create(ORGANIC)
 			.sound(SoundType.STONE)
 			.hardnessAndResistance(1f, 10f)
 	));
+	public static final RegistryObject<Block> FLESHSTONE_BRICKS = BLOCKS.register("fleshstone_bricks", () -> new Block(Block.Properties.create(ORGANIC)
+			.sound(SoundType.STONE)
+			.hardnessAndResistance(1f, 10f)
+	));
+
+	//Tastone Blocks
+	public static final RegistryObject<Block> TASTONE = BLOCKS.register("tastone", () -> new Block(Block.Properties.create(ROCK)
+			.sound(SoundType.STONE)
+			.hardnessAndResistance(2f, 40f)
+	));
+	public static final RegistryObject<Block> TASTONE_BRICKS = BLOCKS.register("tastone_bricks", () -> new Block(Block.Properties.create(ROCK)
+			.sound(SoundType.STONE)
+			.hardnessAndResistance(2f, 40f)
+	));
+
+	//DeadBone
+	public static final RegistryObject<Block> DREADBONE = BLOCKS.register("dreadbone", () -> new Block(Block.Properties.create(ORGANIC)
+			.sound(SoundType.STONE)
+			.hardnessAndResistance(1f, 10f)
+	));
+	public static final RegistryObject<Block> DREADBONE_BLOCK = BLOCKS.register("dreadbone_block", () -> new Block(Block.Properties.create(ORGANIC)
+			.sound(SoundType.STONE)
+			.hardnessAndResistance(1f, 10f)
+	));
+	public static final RegistryObject<Block> DREADBONE_BRICKS = BLOCKS.register("dreadbone_bricks", () -> new Block(Block.Properties.create(ORGANIC)
+			.sound(SoundType.STONE)
+			.hardnessAndResistance(1f, 10f)
+	));
+	public static final RegistryObject<Block> DREADBONE_BRICKS_SMOOTH = BLOCKS.register("dreadbone_bricks_smooth", () -> new Block(Block.Properties.create(ORGANIC)
+			.sound(SoundType.STONE)
+			.hardnessAndResistance(1f, 10f)
+	));
+
+	//HellStone Blocks
 	public static final RegistryObject<Block> HELLSTONE = BLOCKS.register("hellstone", () -> new Block(Block.Properties.create(ROCK)
 			.sound(SoundType.STONE)
 			.hardnessAndResistance(1.5f, 30f)
@@ -47,10 +97,11 @@ public class VillMarkBlocks {
 			.sound(SoundType.STONE)
 			.hardnessAndResistance(1.5f, 30f)
 	));
-	public static final RegistryObject<Block> TASTONE = BLOCKS.register("tastone", () -> new Block(Block.Properties.create(ROCK)
+	public static final RegistryObject<Block> HELLSTONE_BRICKS = BLOCKS.register("hellstone_bricks", () -> new Block(Block.Properties.create(ROCK)
 			.sound(SoundType.STONE)
-			.hardnessAndResistance(2f, 40f)
+			.hardnessAndResistance(1.5f, 30f)
 	));
+
 
 	//Shival Blocks
 	public static final RegistryObject<Block> SHIVAL_FLOOR = BLOCKS.register("shival_floor", () -> new Block(Block.Properties.create(ROCK)
@@ -85,34 +136,23 @@ public class VillMarkBlocks {
 			.hardnessAndResistance(1.5f, 12f)
 	));
 
-	//SAND
-
-	public static final RegistryObject<Block> VILLSAND = BLOCKS.register("villsand", () -> new Block(Block.Properties.create(SAND)
-			.sound(SoundType.SAND)
-			.hardnessAndResistance(0.5f, 10f)
-	));
-	public static final RegistryObject<Block> VILLSANDROCK = BLOCKS.register("villsandrock", () -> new Block(Block.Properties.create(ROCK)
-			.sound(SoundType.STONE)
-			.hardnessAndResistance(1.5f, 30f)
-	));
-
 	//ORES
 
-	public static final RegistryObject<Block> OREBORELEC = BLOCKS.register("oreborelec", () -> new Block(Block.Properties.create(ROCK)
+	public static final RegistryObject<Block> BORELEC_ORE = BLOCKS.register("borelec_ore", () -> new Block(Block.Properties.create(ROCK)
 			.sound(SoundType.STONE)
 			.hardnessAndResistance(1.5f, 30f)
 	));
-	public static final RegistryObject<Block> OREMIRRORIS = BLOCKS.register("oremirroris", () -> new Block(Block.Properties.create(ROCK)
+	public static final RegistryObject<Block> MIRRORIS_ORE = BLOCKS.register("mirroris_ore", () -> new Block(Block.Properties.create(ROCK)
 			.sound(SoundType.STONE)
 			.hardnessAndResistance(1.5f, 30f)
 	));
 
 	//MetalBlocks
-	public static final RegistryObject<Block> BLOCKMIRRORIS = BLOCKS.register("blockmirroris", () -> new Block(Block.Properties.create(ROCK)
+	public static final RegistryObject<Block> MIRRORIS_BLOCK = BLOCKS.register("mirroris_block", () -> new Block(Block.Properties.create(ROCK)
 			.sound(SoundType.STONE)
 			.hardnessAndResistance(1.5f, 30f)
 	));
-	public static final RegistryObject<Block> BLOCKBORELEC = BLOCKS.register("blockborelec", () -> new Block(Block.Properties.create(ROCK)
+	public static final RegistryObject<Block> BORELEC_BLOCK = BLOCKS.register("borelec_block", () -> new Block(Block.Properties.create(ROCK)
 			.sound(SoundType.STONE)
 			.hardnessAndResistance(1.5f, 30f)
 	));
@@ -127,10 +167,10 @@ public class VillMarkBlocks {
 		blocklist.add(FLESHSTONE);
 		blocklist.add(DREADBONE);
 		blocklist.add(DRYROCK);
-		blocklist.add(BLOCKBORELEC);
-		blocklist.add(BLOCKMIRRORIS);
-		blocklist.add(OREMIRRORIS);
-		blocklist.add(OREBORELEC);
+		blocklist.add(BORELEC_BLOCK);
+		blocklist.add(MIRRORIS_BLOCK);
+		blocklist.add(MIRRORIS_ORE);
+		blocklist.add(BORELEC_ORE);
 		blocklist.add(VILLSANDROCK);
 		blocklist.add(VILLSAND);
 		blocklist.add(TASTONE);
@@ -139,10 +179,15 @@ public class VillMarkBlocks {
 		blocklist.add(SHIVAL_BRICKS);
 		blocklist.add(SHIVAL_BRICKS_COLORED);
 		blocklist.add(SHIVAL_PILLAR);
-
-
-
-
+		blocklist.add(HELLSTONE_BRICKS);
+		blocklist.add(DREADBONE_BLOCK);
+		blocklist.add(TASTONE);
+		blocklist.add(DREADBONE_BRICKS_SMOOTH);
+		blocklist.add(DREADBONE_BRICKS);
+		blocklist.add(TASTONE_BRICKS);
+		blocklist.add(FLESHSTONE_BRICKS);
+		blocklist.add(DRYROCK_BRICKS);
+		blocklist.add(VILLMARK_BEDROCK);
 
 
 
